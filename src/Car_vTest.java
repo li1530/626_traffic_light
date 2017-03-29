c2c534d0f336c89535c8d5c0d7f08fffc615f3e6
li1530 <lixx1578@umn.edu>
2017-03-29 18:59:10 -0500

    Added simple unit test

--- src/Car_vTest.java
+++ src/Car_vTest.java
@@ -8,22 +8,24 @@ public class Car_vTest {
 
 	@Test
 	public void testCFTAct() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
 	}
 
 	@Test
 	public void testCFTSetDestination() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
 	}
 
 	@Test
 	public void testDFTAct() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
+		Car_h car = new Car_h();
+		assertEquals(-1, car.initDest);
 	}
 
 	@Test
 	public void testDFTSetDestination() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
 	}
 
 }
