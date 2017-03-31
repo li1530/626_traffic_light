5ecdd8b1fdde3c692992748f86351a45a99c9720
Erik Knutson <erikknutson@Eriks-MacBook-Pro.local>
2017-03-30 19:03:04 -0500

    Added initialization to a test
    Initialization required prior to calling Car_h constructor

--- src/Car_hTest.java
+++ src/Car_hTest.java
@@ -2,9 +2,33 @@
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+import greenfoot.platforms.standalone.GreenfootUtilDelegateStandAlone;
+import greenfoot.util.GreenfootUtil;
+
 public class Car_hTest {
+	private Car_h car_h;
+
+	@Before
+	public void before() {
+		GreenfootUtilDelegateStandAlone delegate = new GreenfootUtilDelegateStandAlone(false, // boolean storageStandalone,
+	            "", // String storageHost, 
+	            "", // String storagePort,
+	            "", // String storagePasscode,
+	            "", // String storageScenarioId,
+	            "", // String storageUserId,
+	            ""); // String storageUserName);
+		GreenfootUtil.initialise(delegate);
+		car_h = new Car_h();
+	}
+
+	@After
+	public void after() {
+		car_h = null;
+	}
 
 	@Test
 	public void testCFTAct() {
@@ -13,7 +37,37 @@ public class Car_hTest {
 
 	@Test
 	public void testCFTSetDestination() {
-		fail("Not yet implemented");
+		// Three Control Flow paths
+		// 1 is false / false
+		// 2 is true / false
+		// 3 is false / true
+		
+		// CF 1
+		car_h.setRotation(0);
+		car_h.setDestination(0);
+		assertTrue("Destination should still be 0, but is: " + car_h.dest, car_h.dest == 0);
+		assertTrue("Rotation should still be 0, but is: " + car_h.getRotation(), car_h.getRotation() == 0);
+		assertTrue("Max destination should still be 0, but is: " + car_h.maxDest, car_h.maxDest == 0);
+		
+		after();
+		before();
+
+		// CF 2
+		car_h.setRotation(0);
+		car_h.setDestination(1);
+		assertTrue("Destination should still be 1, but is: " + car_h.dest, car_h.dest == 1);
+		assertTrue("Rotation should still be 0, but is: " + car_h.getRotation(), car_h.getRotation() == 0);
+		assertTrue("Max destination should have changed to -90, but is: " + car_h.maxDest, car_h.maxDest == -90);
+		
+		after();
+		before();
+
+		// CF 3
+		car_h.setRotation(0);
+		car_h.setDestination(3);
+		assertTrue("Destination should still be 3, but is: " + car_h.dest, car_h.dest == 3);
+		assertTrue("Rotation should still be 0, but is: " + car_h.getRotation(), car_h.getRotation() == 0);
+		assertTrue("Max destination should have changed to 90, but is: " + car_h.maxDest, car_h.maxDest == 90);
 	}
 
 	@Test
