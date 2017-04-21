e5dda0a981fc1b245920e09700edd92fd2056df9
li1530 <lixx1578@umn.edu>
2017-04-20 19:22:26 -0500

    Added all-def Data flow testing for LightAlgorithm

--- src/LightAlgorithmTest.java
+++ src/LightAlgorithmTest.java
@@ -8,82 +8,102 @@ public class LightAlgorithmTest {
 
 	@Test
 	public void testCFTLightAlgorithm() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
 	}
 
 	@Test
 	public void testCFTActivate() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
 	}
 
 	@Test
 	public void testCFTDeactivate() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
 	}
 
 	@Test
 	public void testCFTIsActivated() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
 	}
 
 	@Test
 	public void testCFTResetLightInt() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
 	}
 
 	@Test
 	public void testCFTGetLight() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
 	}
 
 	@Test
 	public void testCFTNextLight() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
 	}
 
 	@Test
 	public void testCFTResetLight() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
 	}
 
 	@Test
 	public void testDFTLightAlgorithm() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
 	}
 
 	@Test
 	public void testDFTActivate() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
 	}
 
 	@Test
 	public void testDFTDeactivate() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
 	}
 
 	@Test
 	public void testDFTIsActivated() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
 	}
 
 	@Test
 	public void testDFTResetLightInt() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
 	}
 
 	@Test
 	public void testDFTGetLight() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
 	}
 
+	/**
+	 * TC-A1: 1-2-4
+	 */
 	@Test
-	public void testDFTNextLight() {
-		fail("Not yet implemented");
+	public void testDFTNextLight1() {
+		//fail("Not yet implemented");
+		LightAlgorithm la = new LightAlgorithm(1, 1);
+		la.activate();
+		la.nextLight();
+		assertEquals(0, la.getLight());
+	}
+	
+	/**
+	 * TC-A1: 1-2-3
+	 */
+	@Test
+	public void testDFTNextLight2() {
+		//fail("Not yet implemented");
+		LightAlgorithm la = new LightAlgorithm(2, 2);
+		la.activate();
+		la.resetLight(1);
+		la.nextLight();
+		assertEquals(2, la.getLight());
 	}
 
 	@Test
 	public void testDFTResetLight() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
 	}
 
 }
